cd949d4960c622f6503a5c695feaf054ceda80f6
Lukasz Mroz <lukasz1mroz@gmail.com>
2023-11-30 17:59:56 +0100

    🎨 Remove old code structure

--- backend/src/query-handler.ts
+++
@@ -1,7 +0,0 @@
-import {PromptTemplate} from 'langchain/prompts'
-
-export const getPromptTemplateWithContext = () => {
-  const combineDocsMockTemplate = `Use the following pieces of context to answer the question at the end. If you don't know the answer, just say that you don't know, don't try to make up an answer. Context: {context} Question: {question}`
-  const combineDocsPrompt = PromptTemplate.fromTemplate(combineDocsMockTemplate)
-  return combineDocsPrompt
-}
