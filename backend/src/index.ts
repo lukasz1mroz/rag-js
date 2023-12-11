d53b8245d10400a543e9efbff7a47acee4aaeadb
Lukasz Mroz <lukasz1mroz@gmail.com>
2023-12-11 13:13:27 +0100

    🎨 Move config and index files

---
+++ backend/src/index.ts
@@ -0,0 +1,12 @@
+//import {getLlmRespChain} from './src/chains/llmRespChain'
+import {loadFileChain} from './chains/loadFileChain'
+
+const main = async (question: string, fileUrl: string): Promise<any> => {
+  // Chunk the file and upload to Redis vector store
+  await loadFileChain.invoke(fileUrl)
+
+  // Get the chain and run it with question
+  //await getLlmRespChain.invoke({fileUrl, question})
+}
+
+await main('Test', './sample-logs/log-sample-1.log')
