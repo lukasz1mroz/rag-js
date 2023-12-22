f907584d73ff43a7ca05eeabd13d851c7970b34e
Lukasz Mroz <lukasz1mroz@gmail.com>
2023-12-22 13:31:56 +0100

    ♻️ Remove comments

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,12 @@
-//import {getLlmRespChain} from './src/chains/llmRespChain'
+import {getLlmRespChain} from './chains/llmRespChain.js'
 import {loadFileChain} from './chains/loadFileChain.js'
 
 const main = async (question: string, fileUrl: string): Promise<any> => {
   // Chunk the file and upload to Redis vector store
   await loadFileChain.invoke(fileUrl)
 
-  // Get the chain and run it with question
-  //await getLlmRespChain.invoke({fileUrl, question})
+  // Get the chain and run it with questions
+  await getLlmRespChain.invoke({fileUrl, question})
 }
 
 await main('Test', './sample-logs/log-sample-1.log')
