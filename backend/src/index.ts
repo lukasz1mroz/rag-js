24dec6893684417c2e2e8611e9e059f35acdb9cd
Lukasz Mroz <lukasz1mroz@gmail.com>
2024-01-18 18:19:06 +0100

    ♻️ Refactor main chain

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,6 @@
-import {getLlmRespChain} from './chains/llmRespChain.js'
-import {loadFileChain} from './chains/loadFileChain.js'
+import {ragChain} from './ragChain.js'
 
-const main = async (question: string, fileUrl: string): Promise<any> => {
-  // Chunk the file and upload to Redis vector store
-  await loadFileChain.invoke(fileUrl)
-
-  // Get the chain and run it with questions
-  await getLlmRespChain.invoke({fileUrl, question})
-}
-
-await main('Test', './sample-logs/log-sample-1.log')
+await ragChain.invoke({
+  filePath: '/Users/mrozl/Documents/Code/Exercises/rag-js/backend/src/sample-logs/log-sample-1.log',
+  question: 'What is the error?',
+})
