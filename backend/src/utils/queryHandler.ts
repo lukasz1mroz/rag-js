b3d7d031839f89482e99827d59c05bf99955cef3
Lukasz Mroz <lukasz1mroz@gmail.com>
2024-02-01 15:48:58 +0100

    ♻️ Refine error prompt

--- backend/src/utils/queryHandler.ts
+++ backend/src/utils/queryHandler.ts
@@ -1,5 +1,19 @@
 import {PromptTemplate} from 'langchain/prompts'
 
-const ragTemplate = `Use the following pieces of context to answer the question at the end. If you don't know the answer, don't make up the answer. Context: {context} Question: {question}`
+const errorLogTemplate =
+  "Role: You are log analyst helping to spot the errors in application logs. \
+  Task: write extensive reports for support teams describing the errors based on provided JSON objects with error data. \
+  The report structure must contain only the below sections: \
+  - 'Error Description' section will be an extensive description of the error and include all the details from relevant error object.  \
+  - 'Error Timeframe' section will be a descriptive paragraph including exact error dates and failing components' names. \
+  - 'Traffic Data' section will always provide exact numbers numbers of error occurences, endpoint calls and endpoints response times. \
+  - 'Log Snippets' section will include only relevant unformatted log objects. \
+  - 'Suggested Next Steps' section will be an exhaustive '-' bulleted list of remediation steps including application code changes, network components checkups and systems and databases checkups involving respective teams. \
+  Remember to: \
+  - Provide the answers based only on real log data except 'Suggested Next Steps' section. \
+  - Always treat log objects with the same transaction id as one error trace. \
+  - Generate separate reports only if multiple errors occurr. \
+  Logs: {logs}\
+"
 
-export const ragFinalPrompt = PromptTemplate.fromTemplate(ragTemplate)
+export const errorLogFinalPrompt = PromptTemplate.fromTemplate(errorLogTemplate)
